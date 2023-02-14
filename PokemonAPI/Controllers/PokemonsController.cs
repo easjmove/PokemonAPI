c61bdf43f3353546697001511c56c3a50e52698b
move <move@zealand.dk>
2023-02-14 09:17:27 +0100

    Add project files.

---
+++ PokemonAPI/Controllers/PokemonsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonAPI.Models;
+using PokemonAPI.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PokemonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    //URI: api/pokemons
+    [ApiController]
+    public class PokemonsController : ControllerBase
+    {
+        private PokemonsRepository _repository;
+
+        public PokemonsController(PokemonsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/<PokemonsController>
+        [HttpGet]
+        public IEnumerable<Pokemon> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        // GET api/<PokemonsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PokemonsController>
+        [HttpPost]
+        public Pokemon Post([FromBody] Pokemon newPokemon)
+        {
+            return _repository.Add(newPokemon);
+        }
+
+        // PUT api/<PokemonsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PokemonsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PokemonAPI/Repositories/PokemonsRepository.cs
@@ -0,0 +1,34 @@
+﻿using PokemonAPI.Models;
+
+
+namespace PokemonAPI.Repositories
+{
+    public class PokemonsRepository
+    {
+        private int _nextID;
+        private List<Pokemon> _pokemons;
+
+        public PokemonsRepository() 
+        {
+            _nextID = 1;
+            _pokemons = new List<Pokemon>()
+            {
+                new Pokemon() {Id = _nextID++, Name="Pikachu", Level=9999, PokeDex=25},
+                new Pokemon() {Id = _nextID++, Name="Charmander", Level=1000, PokeDex=12},
+                new Pokemon() {Id = _nextID++, Name="Arbok", Level=20, PokeDex=80},
+            };
+        }
+
+        public List<Pokemon> GetAll()
+        {
+            return new List<Pokemon>(_pokemons);
+        }
+
+        public Pokemon Add(Pokemon newPokemon)
+        {
+            newPokemon.Id = _nextID++;
+            _pokemons.Add(newPokemon);
+            return newPokemon;
+        }
+    }
+}
